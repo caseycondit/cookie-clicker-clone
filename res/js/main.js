4b97ae2c52b0c77cb7a64e441ff8480cdadeb711
ttoomas <folprecht.to@gmail.com>
2022-11-01 20:49:18 +0100

    cursors around cookie

--- res/js/main.js
+++ res/js/main.js
@@ -8,3 +8,77 @@ cookie.addEventListener('mouseenter', () => {
 cookie.addEventListener('mouseleave', () => {
     cookie.style.animation = "cookieHoverOut 500ms linear forwards";
 })
+
+// Cookie upgrade cursor around cookie
+const cookieUpgradeContainer = document.querySelector('.cookie__upgradesContainer');
+let cookieSize = 256;
+let numberOfBoxes = 20;
+let cookieId = 0;
+
+window.onload = () => {
+    // Create boxes for cursors
+    for (let i = 0; i < numberOfBoxes; i++) {
+        let newCursorBx = document.createElement("div");
+        newCursorBx.classList.add('cookie__upgradesBx');
+    
+        cookieUpgradeContainer.appendChild(newCursorBx);
+    }
+
+    let cookieUpgradeBxs = document.querySelectorAll('.cookie__upgradesBx');
+    
+    cookieUpgradeBxs.forEach((cookieUpgradeBx, cookieIndex) => {
+        // Create cursors
+        let numberOfCursors = 50 + (cookieIndex * 2);
+
+        let div = 360 / numberOfCursors;
+        let radius = 150 + (cookieIndex * 20);
+        let offsetToParentCenter = parseInt(cookieUpgradeBx.offsetWidth / 2);//assumes parent is square
+        let offsetToChildCenter = 10;
+        let totalOffset = offsetToParentCenter - offsetToChildCenter;
+        let angles = [285];
+        let angleStep = 360 / (numberOfCursors + 1);
+        let index = 0;
+    
+        for (let i = 0; i < numberOfCursors; i++) {
+            index++
+            let newImg = document.createElement('img');
+            newImg.src = "./res/img/cursor-pointer.png";
+            newImg.setAttribute('draggable', "false");
+            newImg.classList.add("cookie__upgrade", `id${cookieId}`);
+            cookieUpgradeBx.appendChild(newImg);
+            
+            let y = Math.sin((div * index) * (Math.PI / 180)) * radius;
+            let x = Math.cos((div * index) * (Math.PI / 180)) * radius;
+            newImg.style.top = (y + totalOffset).toString() + "px";
+            newImg.style.left = (x + totalOffset).toString() + "px";
+            
+            newImg.style.rotate = angles[i] + "deg";
+            newImg.style.display = "none";
+            angles.push((angles[i] + angleStep) % 360);
+            cookieId++;
+        }
+
+        let allCursorsInBx = cookieUpgradeBx.querySelectorAll('.cookie__upgrade');
+        let cursorsIndex = 0;
+        let halfCursorIndex = Math.round(allCursorsInBx.length / 2);
+
+        allCursorsInBx[cursorsIndex].classList.add('active');
+        allCursorsInBx[halfCursorIndex].classList.add('active');
+
+        setInterval(() => {
+            allCursorsInBx[cursorsIndex].classList.remove('active');
+            allCursorsInBx[halfCursorIndex].classList.remove('active');
+
+            cursorsIndex++;
+            halfCursorIndex++;
+
+            if(cursorsIndex === allCursorsInBx.length) cursorsIndex = 0;
+            if(halfCursorIndex === allCursorsInBx.length) halfCursorIndex = 0;
+
+            allCursorsInBx[cursorsIndex].classList.add('active');
+            allCursorsInBx[halfCursorIndex].classList.add('active');
+        }, 300);
+    })
+
+    const one = document.querySelector('.cookie__upgrade.id0').style.display = "block";
+}
