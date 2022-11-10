c1526b1adbcf2dabf033653b76f41fa57c2ca3c7
Tomáš Folprecht <tomas.folprecht@student.spsmb.cz>
2022-11-10 13:49:59 +0100

    all buildings generated, checkenableditems

--- res/js/main.js
+++ res/js/main.js
@@ -194,7 +194,7 @@ let clickInSound = new Audio('./res/img/sounds/clickIn.mp3');
 let clickOutSound = new Audio('./res/img/sounds/clickOut.mp3');
 clickInSound.volume = 0.16;
 clickOutSound.volume = 0.16;
-let cookieClickStep = 500;
+let cookieClickStep = 500000000000;
 
 cookie.addEventListener('mousedown', (e) => {
     cookie.style.animation = "cookieHoverOut 400ms linear forwards";
@@ -211,6 +211,7 @@ cookie.addEventListener('mouseup', (e) => {
 
 function cookieClickIncrease(){
     instaCookieCount += cookieClickStep;
+    console.log(instaCookieCount);
     checkEnabledItems();
 
     let intervalI = 0;
@@ -299,7 +300,23 @@ function cookieClickEffect(e){
 const itemBx = document.querySelector('.buy__itemBx');
 const itemAutoClick = document.querySelector('.itemAutoClick');
 const itemGrandma = document.querySelector('.itemGrandma');
-let itemFarm;
+let itemFarm,
+    itemMine,
+    itemFactory,
+    itemBank,
+    itemTemple,
+    itemWizard,
+    itemShip,
+    itemAlchemy,
+    itemPortal,
+    itemTime,
+    itemCondenser,
+    itemPrism,
+    itemChance,
+    itemEngine,
+    itemJsconsole,
+    itemIdle,
+    itemCortex;
 
 function checkEnabledItems(){
     // Autoclick
@@ -316,6 +333,9 @@ function checkEnabledItems(){
     else if(instaCookieCount >= 100 && itemGrandma.classList.contains('itemDisabled')){
         itemGrandma.classList.remove('itemDisabled');
 
+        itemBx.insertAdjacentHTML('beforeend', newMineHtml);
+        itemMine = itemBx.querySelector('.itemMine');
+
         buildingsMoovingDesc();
     }
 
@@ -323,6 +343,161 @@ function checkEnabledItems(){
     else if(instaCookieCount >= 1100 && itemFarm.classList.contains('itemDisabled')){
         itemFarm.classList.remove('itemDisabled');
 
+        itemBx.insertAdjacentHTML('beforeend', newFactoryHtml);
+        itemFactory = itemBx.querySelector('.itemFactory');
+
+        buildingsMoovingDesc();
+    }
+
+    // Mine
+    else if(instaCookieCount >= 12000 && itemMine.classList.contains('itemDisabled')){
+        itemMine.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newBankHtml);
+        itemBank = itemBx.querySelector('.itemBank');
+
+        buildingsMoovingDesc();
+    }
+
+    // Factory
+    else if(instaCookieCount >= 130000 && itemFactory.classList.contains('itemDisabled')){
+        itemFactory.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newTempleHtml);
+        itemTemple = itemBx.querySelector('.itemTemple');
+
+        buildingsMoovingDesc();
+    }
+
+    // Bank
+    else if(instaCookieCount >= 1400000 && itemBank.classList.contains('itemDisabled')){
+        itemBank.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newWizardHtml);
+        itemWizard = itemBx.querySelector('.itemWizard');
+
+        buildingsMoovingDesc();
+    }
+
+    // Temple
+    else if(instaCookieCount >= 20000000 && itemTemple.classList.contains('itemDisabled')){
+        itemTemple.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newShipHtml);
+        itemShip = itemBx.querySelector('.itemShip');
+
+        buildingsMoovingDesc();
+    }
+
+    // Wizard
+    else if(instaCookieCount >= 330000000 && itemWizard.classList.contains('itemDisabled')){
+        itemWizard.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newAlchemyHtml);
+        itemAlchemy = itemBx.querySelector('.itemAlchemy');
+
+        buildingsMoovingDesc();
+    }
+
+    // Ship
+    else if(instaCookieCount >= 5100000000 && itemShip.classList.contains('itemDisabled')){
+        itemShip.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newPortalHtml);
+        itemShip = itemBx.querySelector('.itemShip');
+
+        buildingsMoovingDesc();
+    }
+
+    // Alchemy
+    else if(instaCookieCount >= 75000000000 && itemAlchemy.classList.contains('itemDisabled')){
+        itemAlchemy.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newTimeHtml);
+        itemTime = itemBx.querySelector('.itemTime');
+
+        buildingsMoovingDesc();
+    }
+
+    // Portal
+    else if(instaCookieCount >= 1000000000000 && itemPortal.classList.contains('itemDisabled')){
+        itemPortal.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newCondenserHtml);
+        itemCondenser = itemBx.querySelector('.itemCondenser');
+
+        buildingsMoovingDesc();
+    }
+
+    // Time
+    else if(instaCookieCount >= 14000000000000 && itemTime.classList.contains('itemDisabled')){
+        itemTime.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newPrismHtml);
+        itemPrism = itemBx.querySelector('.itemPrism');
+
+        buildingsMoovingDesc();
+    }
+
+    // Consender
+    else if(instaCookieCount >= 170000000000000 && itemCondenser.classList.contains('itemDisabled')){
+        itemCondenser.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newChanceHtml);
+        itemChance = itemBx.querySelector('.itemChance');
+
+        buildingsMoovingDesc();
+    }
+
+    // Prism
+    else if(instaCookieCount >= 2100000000000000 && itemPrism.classList.contains('itemDisabled')){
+        itemPrism.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newEngineHtml);
+        itemEngine = itemBx.querySelector('.itemEngine');
+
+        buildingsMoovingDesc();
+    }
+
+    // Chance
+    else if(instaCookieCount >= 26000000000000000 && itemChance.classList.contains('itemDisabled')){
+        itemChance.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newJsconsoleHtml);
+        itemJsconsole = itemBx.querySelector('.itemJsconsole');
+
+        buildingsMoovingDesc();
+    }
+
+    // Engine
+    else if(instaCookieCount >= 310000000000000000 && itemEngine.classList.contains('itemDisabled')){
+        itemEngine.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newIdleHtml);
+        itemIdle = itemBx.querySelector('.itemIdle');
+
+        buildingsMoovingDesc();
+    }
+
+    // Jsconsole
+    else if(instaCookieCount >= 71000000000000000000 && itemJsconsole.classList.contains('itemDisabled')){
+        itemJsconsole.classList.remove('itemDisabled');
+
+        itemBx.insertAdjacentHTML('beforeend', newCortexHtml);
+        itemCortex = itemBx.querySelector('.itemCortex');
+
+        buildingsMoovingDesc();
+    }
+
+    // Idle
+    else if(instaCookieCount >= 12000000000000000000000 && itemIdle.classList.contains('itemDisabled')){
+        itemIdle.classList.remove('itemDisabled');
+        buildingsMoovingDesc();
+    }
+
+    // Cortex
+    else if(instaCookieCount >= 1900000000000000000000000 && itemCortex.classList.contains('itemDisabled')){
+        itemCortex.classList.remove('itemDisabled');
         buildingsMoovingDesc();
     }
 }
@@ -333,27 +508,43 @@ let cursorPrize = 15;
 let grandmaPrize = 100;
 let farmPrize = 1100;
 let minePrize = 12000;
+let factoryPrize = 130000;
+let bankPrize = 1400000;
+let templePrize = 20000000;
+let wizardPrize = 330000000;
+let shipPrize = 5100000000;
+let alchemyPrize = 75000000000;
+let portalPrize = 1000000000000;
+let timePrize = 14000000000000;
+let condenserPrize = 170000000000000;
+let prismPrize = 2100000000000000;
+let chancePrize = 26000000000000000;
+let enginePrize = 310000000000000000;
+let jsconsolePrize = 71000000000000000000;
+let idlePrize = 12000000000000000000000;
+let cortexPrize = 1900000000000000000000000;
 
 let buildPrizes = [cursorPrize, grandmaPrize, farmPrize];
 
-const newFarmHtml = generateNewBuilding("Farma", "Farm", farmPrize.toLocaleString('de-DE'), "Tady se z keksových semínek pěstují keksové rostliny.", "234.4", 3);
-const newMineHtml = generateNewBuilding("Důl", "Mine", "12,000", "Těží sušenkové těsto a čokoládové střípky.", "1,065", 4);
-const newFactoryHtml = generateNewBuilding("Továrna", "Factory", "130,000", "Produkuje velké množství keksů.", "5,893", 5);
-// toLocalString(de-DE) - dot instead of comma
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
-// const newMineHtml = generateNewBuilding("Důl", "mine", "12,000", ".", "234.4", 3);
+const newFarmHtml = generateNewBuilding("Farma", "Farm", farmPrize.toLocaleString('en-Us'), "Tady se z keksových semínek pěstují keksové rostliny.", "234.4", 3, -101);
+const newMineHtml = generateNewBuilding("Důl", "Mine", minePrize.toLocaleString('en-Us'), "Těží sušenkové těsto a čokoládové střípky.", "1,065", 4, -148);
+const newFactoryHtml = generateNewBuilding("Továrna", "Factory", factoryPrize.toLocaleString('en-Us'), "Produkuje velké množství keksů.", "5,893", 5, -196);
+const newBankHtml = generateNewBuilding("Banka", "Bank", bankPrize.toLocaleString('de-De'), "Generuje keksy z úroku.", "31,733", 6, -726);
+const newTempleHtml = generateNewBuilding("Chrám", "Temple", templePrize.toLocaleString('de-De'), "Plné vzácné, starodávné čokolády.", "176,802", 7, -774);
+const newWizardHtml = generateNewBuilding("Čarodějná věž", "Wizard", wizardPrize.toLocaleString('de-De'), "Vyvolává keksy pomocí kouzel.", "997,347", 8, -821);
+const newShipHtml = generateNewBuilding("Dodávka", "Ship", shipPrize.toLocaleString('de-De'), "Přiváží čerstvé keksy z keksové planety.", "2.947 milion", 9, -244);
+const newAlchemyHtml = generateNewBuilding("Alchymistická laboratoř", "Alchemy", alchemyPrize.toLocaleString('de-De'), "Přetváří zlato na keksy.", "36.267 milion", 10, -292);
+const newPortalHtml = generateNewBuilding("Portál", "Portal", portalPrize.toLocaleString('de-De'), "Otevírá brány do Keksmíru.", "113.335 milion", 11, -340);
+const newTimeHtml = generateNewBuilding("Stroj času", "Time", timePrize.toLocaleString('de-De'), "Přinášejte keksy z minulosti, které ještě nikdo nesnědl.", "736.677 milion", 12, -387);
+const newCondenserHtml = generateNewBuilding("Antihmotový kondenzátor", "Condenser", condenserPrize.toLocaleString('de-De'), "Kondenzuje antihmotu ve vesmíru na keksy.", "4.873 bilion", 13, -628);
+const newPrismHtml = generateNewBuilding("Prizma", "Prism", prismPrize.toLocaleString('de-De'), "Samotné světlo přeměňuje na keksy.", "32.867", 14, -674);
+const newChanceHtml = generateNewBuilding("Šancovač", "Chance", chancePrize.toLocaleString('de-De'), "Generuje keksy jen tak z ničeho, čistě na bázi štěstí.", "238.003 bilion", 15, -914);
+const newEngineHtml = generateNewBuilding("Fraktální motor", "Engine", enginePrize.toLocaleString('de-De'), "Přeměňuje keksy na ještě více keksů.", "1.7 trillion", 16, -966);
+const newJsconsoleHtml = generateNewBuilding("Konzole javascript", "Jsconsole", jsconsolePrize.toLocaleString('de-De'), "Vytváří keksy přímo v kódu, ve kterém je psána tato hra.", "6.233 trillion", 17, -1541);
+const newIdleHtml = generateNewBuilding("Nečinný vesmír", "Idle", idlePrize.toLocaleString('de-De'), "Vedle našeho vesmíru existuje i několik dalších, nečinných vesmírů. Konečně jste objevili způsob, jak se nabourat do jejich produkce a cokoliv, co vyrábějí, přeměnit na keksy.", "47.034 trillion", 18, -1590);
+const newCortexHtml = generateNewBuilding("Kortexový pekař", "Cortex", cortexPrize.toLocaleString('de-De'), "Tyto umělé mozky o velikosti planety jsou schopné zhmotnit sny o keksech. Čas a prostor jsou nepodstatné. Realita je svévolná.", "101.506 septillion", 19, -1637);
 
-function generateNewBuilding(name, buildClass, prize, citation, productionPerSec, buldingIndex){
+function generateNewBuilding(name, buildClass, prize, citation, productionPerSec, buldingIndex, descIconPos){
     return (`
         <div class="buy__item item${buildClass} itemDisabled">
             <img src="./res/img/buildings.png" alt="" style="object-position: 0px -${buldingIndex * 64}px;" class="item__icon" draggable="false">
@@ -367,7 +558,7 @@ function generateNewBuilding(name, buildClass, prize, citation, productionPerSec
             <h4 class="item__count"></h4>
             <div class="item__desc">
                 <div class="desc__top">
-                    <img src="./res/img/icons.png" alt="" style="object-position: -${(buldingIndex - 1) * 50}px 0px;" class="desc__icon" draggable="false">
+                    <img src="./res/img/icons.png" alt="" style="object-position: ${descIconPos}px 0px;" class="desc__icon" draggable="false">
                     <div class="desc__nameBx">
                         <p class="desc__name">???</p>
                         <p class="desc__name descNameDis">${name}</p>
